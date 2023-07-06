2a1065c021f2865d8a62f69d49d81d9d1fcf90fe
jonathan_baraza <jbaraza46@gmail.com>
2023-07-06 20:37:08 +0300

    load about window without menu

--- main.js
+++ main.js
@@ -20,6 +20,17 @@ function createMainWindow() {
   mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
 
+//create About window
+function createAboutWindow() {
+  const aboutWindow = new BrowserWindow({
+    title: "About",
+    height: 500,
+    width: 500,
+  });
+  aboutWindow.removeMenu();
+  aboutWindow.loadFile(path.join(__dirname, "./renderer/about.html"));
+}
+
 //App is ready
 app
   .whenReady()
@@ -64,16 +75,19 @@ const menu = [
   {
     role: "fileMenu",
   },
-  ...(!isMac?[
-    {
-      label:"Help",
-      submenu:[
+  ...(!isMac
+    ? [
         {
-          label:"About"
-        }
+          label: "Help",
+          submenu: [
+            {
+              label: "About",
+              click: () => createAboutWindow(),
+            },
+          ],
+        },
       ]
-    }
-  ]:[])
+    : []),
 ];
 
 //Mac behaves differently so you have to close the app when all windows are closed
